221af4c3ec48ddca47459ffb4a38d7f0c91bcae7
Clint Walker <cw@neonrelic.com>
2017-08-21 15:28:56 +0100

    Rename `api` references in core to `cox`

--- src/core.js
+++ src/core.js
@@ -18,7 +18,7 @@ const error = require('./error')
 
 /**
   Ultra thin abstraction to run a chain of functions
-  Runs `fns` as a Promise chain, passing the config `api` as the first param
+  Runs `fns` as a Promise chain, passing the config `ctx` as the first param
   and the `out` output of the previous Promise as the second.
   `fns` can return synchronously or with a Promise. The chain waits while
   until the Promise returns
@@ -26,22 +26,22 @@ const error = require('./error')
    - ORDER matters! Be careful
    - The value of `out` CHANGES EVERY call (being the output of the previous)
 
-  @param {Function[]} fns Array of functions to run (each passed `(api, out)`)
-  @param {Object} api The API block definition to pass to `fns`
+  @param {Function[]} fns Array of functions to run (each passed `(ctx, out)`)
+  @param {Object} ctx The Context definition to pass to `fns`
   @param {Mixed} [initial] Optional initial data to pass to first fn in list
 
   @return {Mixed} The output of the last function in `fns`
   @private
 */
 
-const step = (fns, api, initial = null) => {
+const step = (fns, ctx, initial = null) => {
   return Promise.reduce(fns, (out, fn) => {
     // Sanity check that `out` isn't an uninvoked function
     // (Can happen when nesting middleware functions incorrectly)
     if (typeof out === 'function') {
       const errMsg = 'Fage middlware chain returned a function'
       const debug = {
-        path: api.path,
+        path: ctx.path,
         details: 'Middleware in `fns` chain must return a value not a ' +
           'function. Probably a nested middleware missing an invocation.'
       }
@@ -53,11 +53,11 @@ const step = (fns, api, initial = null) => {
       })
     }
 
-    return fn(api, out)
+    return fn(ctx, out)
   }, initial)
     // Catch errors and apply any 'onError' handler if present
     .catch(error => {
-      if (api.onError) api.onError(api, error)
+      if (ctx.onError) ctx.onError(ctx, error)
       // Pass the error along
       throw error
     })
@@ -87,13 +87,13 @@ const lock = obj => {
 
   @param {Function[]} fns Array of functions to run (each passed `(api, out)`)
 
-  @return {Function} Middleware function to use in an API block definition
+  @return {Function} Middleware function to use in a context definition
 */
 
-module.exports.compose = fns => (api, output = null) => {
-  // Note: No need to lock the `api` here, as it is locked in the runner()
+module.exports.compose = fns => (ctx, output = null) => {
+  // Note: No need to lock the `ctx` here, as it is locked in the runner()
   // which is always called when executing the method path
-  const called = Object.assign({}, api)
+  const called = Object.assign({}, ctx)
   delete called.fns
 
   // Filter out "Empty/null/undefined" fns in the array of `fns`
@@ -103,20 +103,20 @@ module.exports.compose = fns => (api, output = null) => {
 /**
   "Runs" an API block definition
 
-  @param {Object} api The API definition block
+  @param {Object} ctx The Context definition block
   @param {Mixed} data Channel for user supplied data
   @param {Object} meta Channel of application supplied data
 
-  @return {Mixed} The output of the `api.fns`
+  @return {Mixed} The output of the `ctx.fns`
 */
 
-module.exports.run = (api, data, meta = {}) => {
-  if (!api.path || !api.fns) throw new Error('bad engine args (path or fns)')
+module.exports.run = (ctx, data, meta = {}) => {
+  if (!ctx.path || !ctx.fns) throw new Error('bad engine args (path or fns)')
 
-  // Enforce immutability of the core `api` definition
-  const kerchink = lock(Object.assign({}, api, {raw: data, meta: meta}))
+  // Enforce immutability of the core `ctx` definition
+  const kerchink = lock(Object.assign({}, ctx, {raw: data, meta: meta}))
   // Setup unlocked state on the context definition
   const context = Object.assign({state: {}}, kerchink)
 
-  return step(api.fns, context)
+  return step(ctx.fns, context)
 }
