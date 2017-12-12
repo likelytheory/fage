8507a6307a2fd4c0c6b990e0c594759c3064e20a
Clint Walker <cw@neonrelic.com>
2017-12-12 19:07:21 +0200

    Update Fage Typescript typings

--- types/fage-tests.ts
+++ types/fage-tests.ts
@@ -1,1 +1,32 @@
 import * as Fage from '../';
+
+Fage.error.badrequest({message: '400'});
+Fage.error.conflict({message: '409'});
+Fage.error.fatal({message: '500'});
+Fage.error.forbidden({message: '403'});
+Fage.error.notfound({message: '404'});
+Fage.error.ratelimit({message: '423'});
+Fage.error.unauthorized({message: '401'});
+Fage.error.unavailable({message: '501'});
+
+Fage.run({
+  path: 'demo',
+  onError: (err) => {},
+  ref: {oh: 'hi'},
+  fns: [
+    () => {}
+  ]
+});
+
+const mb: Fage.MethodBlock = {
+  path: 'go',
+  fns: [
+    () => 'hello'
+  ]
+};
+
+Fage.compose([
+  () => {}
+]);
+
+Fage([mb, {path: 'junk', fns: [() => 'derp']}]);
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,43 +1,68 @@
 export = Fage;
 export as namespace Fage;
 
+declare function Fage(methodBlocksArray: Fage.MethodBlock[]): Fage.RunBlock;
+
 declare namespace Fage {
+  interface RunBlock {
+    [pathKey: string]: (input: any, meta?: object) => any;
+  }
+
   interface ErrorOptions {
-    type?: string,
-    message?: string,
-    code?: number | string,
-    debug?: any,
-    errors?: any
+    type?: string;
+    message?: string;
+    code?: number | string;
+    debug?: any;
+    errors?: any;
   }
 
   interface ErrorOptionsAll {
-    status: number,
-    type: string,
-    message?: string,
-    code?: number | string,
-    debug?: any,
-    errors?: any
+    status: number;
+    type: string;
+    message?: string;
+    code?: number | string;
+    debug?: any;
+    errors?: any;
   }
 
   interface Error {
-    status: number,
-    type: string,
-    message?: string,
-    code?: number | string,
-    debug?: any,
-    errors?: any
+    status: number;
+    type: string;
+    message?: string;
+    code?: number | string;
+    debug?: any;
+    errors?: any;
   }
 
   interface ErrorBuilder {
-    create(options: ErrorOptionsAll) : Error,
+    create(options: ErrorOptionsAll): Error;
 
-    badrequest(opts: ErrorOptions): Error,
-    unauthorized(opts: ErrorOptions): Error,
-    forbidden(opts: ErrorOptions): Error,
-    notfound(opts: ErrorOptions): Error,
-    conflict(opts: ErrorOptions): Error,
-    ratelimit(opts: ErrorOptions): Error,
-    fatal(opts: ErrorOptions): Error,
-    unavailable(opts: ErrorOptions): Error
+    badrequest(opts: ErrorOptions): Error;
+    unauthorized(opts: ErrorOptions): Error;
+    forbidden(opts: ErrorOptions): Error;
+    notfound(opts: ErrorOptions): Error;
+    conflict(opts: ErrorOptions): Error;
+    ratelimit(opts: ErrorOptions): Error;
+    fatal(opts: ErrorOptions): Error;
+    unavailable(opts: ErrorOptions): Error;
   }
+
+  type Middleware = (ctx: object, prevResult?: any) => any;
+
+  interface MethodBlock {
+    path: string;
+    fns: Middleware[];
+    ref?: object;
+    onError?: (error?: Error) => void;
+  }
+
+  const error: ErrorBuilder;
+
+  function run(
+    methodBlock: MethodBlock
+  ): Promise<any>;
+
+  function compose(
+    fns: Middleware[]
+  ): Middleware;
 }
