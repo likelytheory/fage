96d9ad58576ed474ae0dcb69dd44fdf90ce9c55e
Clint Walker <cw@neonrelic.com>
2018-03-14 22:34:29 +0200

    docs: additional tying docs

--- types/index.d.ts
+++ types/index.d.ts
@@ -16,27 +16,27 @@ declare namespace Fage {
     /**
      * `input` data channel: raw user input (untrusted)
      */
-    input: any,
+    input: any;
 
     /**
      * `meta` data channel: trusted data set by the application
      */
-    meta: any,
+    meta: any;
 
     /**
      * The path name of the Fage method block
      */
-    path: string,
+    path: string;
 
     /**
      * Any reference data attached by the Fage method block
      */
-    ref?: any,
+    ref?: any;
 
     /**
      * A mutable state container - set your own data here
      */
-    state?: any
+    state?: any;
   }
 
   interface RunBlock {
@@ -60,6 +60,17 @@ declare namespace Fage {
     errors?: any;
   }
 
+  /**
+   * Fage Errors extend default Javascript Error objects with custom fields
+   * that can provide useful additional information to app environments
+   *
+   * @prop {Number} status
+   * @prop {String} type
+   * @prop {String} [message]
+   * @prop {Number|String} [code]
+   * @prop {any} [debug] A container for any debug info dumps
+   * @prop {any} [errors] A container for errors to attach for reference
+   */
   interface Error {
     status: number;
     type: string;
@@ -72,22 +83,64 @@ declare namespace Fage {
   interface ErrorBuilder {
     create(options: ErrorOptionsAll): Error;
 
+    /** 400 Bad Request */
     badrequest(opts: ErrorOptions): Error;
+    /** 401 Unauthorized */
     unauthorized(opts: ErrorOptions): Error;
+    /** 403 Forbidden */
     forbidden(opts: ErrorOptions): Error;
+    /** 404 Not Found */
     notfound(opts: ErrorOptions): Error;
+    /** 409 Conflict */
     conflict(opts: ErrorOptions): Error;
+    /** 421 Rate Limit */
     ratelimit(opts: ErrorOptions): Error;
+    /** 500 Server Error */
     fatal(opts: ErrorOptions): Error;
+    /** 503 Server Unavailable */
     unavailable(opts: ErrorOptions): Error;
   }
 
+  /**
+   * Fage Middleware are functions that receive the Fage Context object
+   * and the output of the previous middleware (or `undefined` if no previous
+   * function exists). For async functions, return a Promise, otherwise
+   * return any synchronous value. Critical errors should `throw` a
+   * Fage [Error](#Error)
+   *
+   * @param {Fage.Context} ctx The Fage Context object for this method block
+   * @param {any} [prevResult] The result from the previous middleware
+   *
+   * @throws {Error} Critical errors are thrown by Middleware
+   * @returns {any} The result of this middleware function
+   */
   type Middleware = (ctx: Context, prevResult?: any) => any;
 
+  /**
+   * A Fage MethodBlock defines the functions `fns` to run through the Promise
+   * reducer and a `path` name to uniquely identify the block
+   */
   interface MethodBlock {
+    /**
+     * The `path` to uniquely identify the MethodBlock
+     */
     path: string;
+
+    /**
+     * An array of Fage.Middleware functions
+     */
     fns: Middleware[];
+
+    /**
+     * Any reference `ref` data to pass along to Middleware functions
+     */
     ref?: object;
+
+    /**
+     * A custom handler to process any Errors thrown by the Middleware.
+     * Note that this is a pass through function, the thrown Error will
+     * continue to be thrown after being processed by this handler.
+     */
     onError?: (error?: Error) => void;
   }
 
